1126321509a4c0f9b39ee037638b55d2b18bf407
sarang <saranggp2018@gmail.com>
2025-04-18 18:03:58 +0530

    added some fixes
    Signed-off-by: sarang <saranggp2018@gmail.com>

--- src/components/Pricing/PricingBox.tsx
+++ src/components/Pricing/PricingBox.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import OfferList from "./OfferList";
 import { Price } from "@/types/price";
+import { CheckmarkIcon } from "react-hot-toast";
 
 const PricingBox = ({ product }: { product: Price }) => {
   // POST request
@@ -22,13 +23,23 @@ const PricingBox = ({ product }: { product: Price }) => {
   };
 
   return (
-    <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+    <div className={`w-full px-4 md:w-1/2 lg:w-1/3  ${  product.nickname === "Premium"
+      ? "relative z-15 shadow-lg scale-100 animate-[zoomInOut_1s_ease-in-out_infinite]"
+      : "relative z-0 opacity-60 scale-90"}  `}>
+
+<div
+  className={`relative group  ${
+    product.nickname === "Premium"
+      ? "before:absolute before:-inset-0.5 before:rounded-xl before:bg-gradient-to-r before:from-blue-300 before:to-purple-300 before:blur-sm before:opacity-25 before:transition before:duration-300"
+      : ""
+  }`}
+>
       <div
-        className="relative z-10 mb-10 overflow-hidden rounded-xl bg-white px-8 py-10 shadow-[0px_0px_40px_0px_rgba(0,0,0,0.08)] dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-14"
+        className={`relative z-10 mb-10 overflow-hidden rounded-xl bg-white px-8 py-10  shadow-[-10px_-15px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.12)] dark:bg-dark-2 sm:p-12 lg:px-6 lg:py-10 xl:p-14`}
         data-wow-delay=".1s"
       >
         {product.nickname === "Premium" && (
-          <p className="absolute right-[-50px] top-[60px] inline-block -rotate-90 rounded-bl-md rounded-tl-md bg-primary px-5 py-2 text-base font-medium text-white">
+          <p className="absolute right-[-50px] top-[60px] inline-block -rotate-90 rounded-bl-md rounded-tl-md bg-primary px-5 py-2 text-base cursor-default font-medium text-white">
             Recommended
           </p>
         )}
@@ -54,8 +65,13 @@ const PricingBox = ({ product }: { product: Price }) => {
           </h3>
           <div className="mb-10">
             {product?.offers.map((offer, i) => (
+               <div className="flex gap-2">
+               <div>
+        <BlueTick />
+          </div>
               <OfferList key={i} text={offer} />
-            ))}
+            </div>
+             ))}
           </div>
         </div>
         <div className="w-full">
@@ -67,8 +83,26 @@ const PricingBox = ({ product }: { product: Price }) => {
           </button>
         </div>
       </div>
+      </div>
     </div>
   );
 };
+
+
+const BlueTick = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="22"
+    height="22"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#3B82F6" 
+    strokeWidth="3"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M20 6L9 17l-5-5" />
+  </svg>
+);  
 
 export default PricingBox;
--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -19,7 +19,7 @@ const Pricing = () => {
           />
         </div>
 
-        <div className="-mx-4 flex flex-wrap justify-center">
+        <div className="-mx-4 flex flex-wrap  justify-center" >
           {pricingData.map((product, i) => (
             <PricingBox key={i} product={product} />
           ))}     
